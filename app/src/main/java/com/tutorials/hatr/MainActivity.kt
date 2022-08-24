22f49bc7ccf3513a426750f2f080d430afe983be
ChoHJin <cjc9739@naver.com>
2022-08-25 01:58:27 +0900

    Initial commit

---
+++ app/src/main/java/com/tutorials/hatr/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.tutorials.hatr
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*Happy Apple Tree Ricecake shop */
+
+        println("beginning ")
+
+    }
+}
